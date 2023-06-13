533ddc83c3ff79094897409f9cd948b6676a6070
EC2 Default User <ec2-user@ip-172-31-44-229.ap-northeast-1.compute.internal>
2023-06-13 08:35:08 +0000

    6/1317:35

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,40 @@
+class Public::CustomersController < ApplicationController
+
+   def show
+    @customer = Customer.find(params[:id])
+    @illustrations = @customer.illustrations
+    @illustration = Illustration.new
+   end
+
+  def edit
+    @customer = Customer.find(params[:id])
+     if @customer == current_customer
+       render :edit
+     else
+       redirect_to customer_path(current_user)
+     end
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "User-introduction was successfully updated."
+      redirect_to customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @customers = Customer.all
+    @illustration = Illustration.new
+    @customer = current_customer
+
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:name, :introduction, :image)
+    end
+
+end
